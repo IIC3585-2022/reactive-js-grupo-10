d2bc45f0414f2a1955aa4f0194da2f1b780a6f57
dgonzalezg <dmgonzalezg@uc.cl>
2022-04-08 17:03:43 -0400

    add boundries

--- src/canvas.js
+++ src/canvas.js
@@ -1,61 +1,42 @@
-export const COLS          = 15;
-export const ROWS          = 40;
+export const COLS          = 30;
+export const ROWS          = 30;
 export const GAP_SIZE      = 1;
 export const CELL_SIZE     = 10;
 export const CANVAS_WIDTH  = COLS * ( CELL_SIZE + GAP_SIZE );
 export const CANVAS_HEIGHT = ROWS * ( CELL_SIZE + GAP_SIZE );
 
+const dimensionWidth = innerWidth-40
+const dimensionHeight = innerHeight-40
+const offset = 20;
+const spaceSize = 25;
+const drawSquare = (ctx) => {
+    const halfHeight = Math.trunc(dimensionHeight/2)
+    const halfWidth = Math.trunc(dimensionWidth/2)
+    ctx.moveTo(20, 20);
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = 'blue'
+    ctx.strokeRect(20, 20, dimensionWidth, dimensionHeight);
+    // espacio izq
+    ctx.moveTo(20, halfHeight-spaceSize);
+    ctx.lineTo(20,halfHeight+spaceSize)
+    //espacio abajo
+    ctx.moveTo(halfWidth+offset-spaceSize, dimensionHeight+offset);
+    ctx.lineTo(halfWidth+offset+spaceSize,dimensionHeight+offset);
+    //espacio derecha
+    ctx.moveTo(dimensionWidth+offset, halfHeight+offset-25);
+    ctx.lineTo(dimensionWidth+offset,halfHeight+offset+25);
+    //espacio arriba
+    ctx.moveTo(halfWidth+offset-25, offset);
+    ctx.lineTo(halfWidth+offset+25, offset);
+    ctx.strokeStyle = 'black'
+    ctx.stroke();
+    
+}
 export function createCanvasElement() {
     const canvas = document.createElement( 'canvas' );
-    canvas.width = CANVAS_WIDTH;
-    canvas.height = CANVAS_HEIGHT;
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    var ctx = canvas.getContext("2d");
+    drawSquare(ctx)
     return canvas;
-}
-
-export function render( ctx, scene ) {
-    renderBackground( ctx, scene.score );
-    renderPacman( ctx, scene.pacman );
-    renderPoints( ctx, scene.points);
-    renderGhosts( ctx, scene.ghosts);
-}
-
-export function renderBackground( ctx, score ) {
-    ctx.fillStyle = 'rgba( 251,251,251, .2 )';
-    ctx.fillRect( 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT );
-
-    if( !score ) return;
-    ctx.font = '24px serif';
-    ctx.fillStyle = 'green';
-    ctx.fillText( `${score}`, 24, 24 );
-}
-
-export function renderSnake( ctx, snake ) {
-    snake.forEach( ( segment, index ) => paintCell( ctx, wrapBounds( segment ), getSegmentColor( index ) ) );
-}
-
-export function renderApples( ctx, apples ) {
-    points.forEach( point => {
-        paintCell( ctx, point, 'yellow' );
-    } );
-}
-
-export function paintCell( ctx, point, color ) {
-    const x = point.x * ( CELL_SIZE + GAP_SIZE );
-    const y = point.y * ( CELL_SIZE + GAP_SIZE );
-
-    ctx.fillStyle = color;
-    ctx.fillRect( x, y, CELL_SIZE, CELL_SIZE );
-}
-
-export function wrapBounds( point ) {
-    const x = point.x >= COLS ? 0 : point.x < 0 ? COLS - 1 : point.x;
-    const y = point.y >= ROWS ? 0 : point.y < 0 ? ROWS - 1 : point.y;
-
-    point.x = x;
-    point.y = y;
-
-    return point;
-}
-export function checkCollision(a, b) {
-    return a.x === b.x && a.y === b.y;
 }
--- src/main.js
+++ src/main.js
@@ -1,38 +1,8 @@
-
-import { Observable, BehaviorSubject } from 'rxjs';
-
-import { animationFrame } from 'rxjs/scheduler/animationFrame';
-
-import { checkGhostCollision, createCanvasElement, render } from "./canvas";
-import { DIRECTIONS, FPS, POINT_COUNT, GHOST_SPEED, WEAK_SPEED, SPEED } from "./constants";
-import { generateApples, generateSnake, move, nextDirection, eat } from "./utils";
-
-const INITIAL_DIRECTION = DIRECTIONS[ 40 ];
+import { createCanvasElement } from "./canvas";
 
 const canvas = createCanvasElement();
 const ctx = canvas.getContext( '2d' );
 document.body.appendChild( canvas );
+document.body.style.background = 'black';
+document.body.style.margin = 0;
 
-let keyDown$ = Observable.fromEvent( document.body, 'keydown' );
-
-let tick$ = Observable.interval( SPEED );
-
-let direction$ = keyDown$
-    .map( ( e ) => DIRECTIONS[ e.keyCode ] )
-    .filter( direction => !!direction )
-    .startWith( INITIAL_DIRECTION )
-    .scan( nextDirection )
-    .distinctUntilChanged();
-
-
-let scene$ = Observable.combineLatest( pacman$, points$, ghosts$, ( pacman, points, ghosts ) => ({ pacman, points, ghosts }) );
-
-let game$ = Observable.interval( 1000 / FPS, animationFrame )
-    .withLatestFrom( scene$, ( _, scene ) => scene )
-    .takeWhile( scene => checkGhostCollision( scene.pacman, scene.ghosts) )
-;
-
-game$.subscribe( {
-    next: ( scene ) => render( ctx, scene ),
-    complete: console.log
-} );
